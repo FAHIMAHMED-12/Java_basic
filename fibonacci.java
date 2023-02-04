9983b1ff0191defd346c4674555a541fdbecbf99
Fahim Ahmed <fahimaks418@gmail.com>
2023-02-05 00:31:45 +0600

    fibonacci code renamed

--- fibonacci.java
+++ fibonacci.java
@@ -1,16 +1,22 @@
-public class newprog {
+import java.util.Scanner;
+
+public class fibonacci {
     public static void main(String[] args) {
+        
+        Scanner n = new Scanner(System.in);
+        int Num = n.nextInt();
         int a=0;
         int b=1;
         int c;
         System.out.print(a+" "+b);
     
-        for(int i=1; i<=10; i++){
+        for(int i=1; i<=Num; i++){
             c=a+b;
             System.out.print(" "+c);
             a=b;
             b=c;
         }
+        n.close();
     }
     
 }
